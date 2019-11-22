458a90c3a79ed3254cdd023aebc06ebe6e81565c
Nicolas DUCHESNE <niduches@e1r9p18.42.fr>
2019-11-22 22:56:49 +0100

    add map

--- src/bonus/display_bonus.c
+++ src/bonus/display_bonus.c
@@ -6,7 +6,7 @@
 /*   By: niduches <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 14:53:13 by niduches          #+#    #+#             */
-/*   Updated: 2019/11/22 21:56:19 by niduches         ###   ########.fr       */
+/*   Updated: 2019/11/22 22:55:53 by niduches         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -174,10 +174,42 @@ void		update_sprite(t_game *game)
 	}
 }
 
+void		display_map(t_map *map, t_tex *img)
+{
+	int		i;
+	int		j;
+	int		size;
+	int		pos;
+	int		color;
+
+	i = 0;
+	pos = map->res.x / 1.65;
+	size = map->res.x / 150;
+	while (i < map->nb_line && i < 100 && (i + 1) * size < map->res.y)
+	{
+		j = 0;
+		while (j < map->line_size[i] && j < 100 && pos + (j + 1) * size < map->res.x)
+		{
+			color = 0;
+			if (map->map[i][j] == '1' || map->map[i][j] == '6')
+				color = 0x00FFFFFF;
+			else if (map->map[i][j] != '0')
+				color = 0x0000FF00;
+			draw_square(img, (t_vec2i){pos + j * size, i * size},
+(t_vec2i){size, size}, color);
+			j++;
+		}
+		i++;
+	}
+	draw_square(img, (t_vec2i){pos + (int)map->pos.x * size, (int)map->pos.y *
+size}, (t_vec2i){size, size}, 0x00FF0000);
+}
+
 int			display(t_game *game)
 {
 	raycasting(&game->map, &game->img);
 	display_hud(game, &game->img);
+	display_map(&game->map, &game->img);
 
 	mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->img.tex, 0, 0);
 
